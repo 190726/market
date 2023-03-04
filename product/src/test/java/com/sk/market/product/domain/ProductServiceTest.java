711737e1a47361f7f879549ef72004e451f497b8
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-03-04 16:06:15 +0900

    test commit

---
+++ product/src/test/java/com/sk/market/product/domain/ProductServiceTest.java
@@ -0,0 +1,13 @@
+package com.sk.market.product.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductServiceTest {
+
+	@Test
+	void testName() throws Exception {
+		
+	}
+}
